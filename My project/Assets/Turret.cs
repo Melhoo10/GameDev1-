5b5ef951cee7fc6566e889963ab2234e4b58265b
Melhoo10 <144268481+Melhoo10@users.noreply.github.com>
2025-09-11 12:19:59 -0400

    UD

--- My project/Assets/Turret.cs
+++ My project/Assets/Turret.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 
 public class Turret : MonoBehaviour
+    
 
 
 {//shooting 
@@ -13,15 +14,33 @@ public class Turret : MonoBehaviour
     public float rate_of_fire = 0.2f; 
     public float shoot_timer = 0;
 
+  
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
+       
     {
-        
+        //If there is time on the shoot_timer...
+        if (shoot_timer > 0)
+            shoot_timer -= Time.deltaTime; //Subrtact delta time from it. 
+        else //Otherwise. 
+        {
+            //Spawn Bullets
+            //Reset the timer.
+            shoot_timer = rate_of_fire; 
+
+            //Instance a new bullet. 
+            GameObject new_bullet = Instantiate(bullet);
+
+            //Move the bullet to the position of the turret. 
+            new_bullet.transform.position = transform.position;
+        }
+
     }
 }
--- My project/Assets/Vectors/Bullet.cs
+++ My project/Assets/Vectors/Bullet.cs
@@ -1,11 +1,17 @@
+using Mono.Cecil;
 using UnityEngine;
 
+
 public class Bullet : MonoBehaviour
+
+
+
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-  public void SetVelocity(Vector3 direction)
-    { 
-
+  public void SetVelocity(Vector3 direction) 
+    {
+       //Find the direction that the bullets needs to travel in and then pass it to the bullet.
+       Vector3 bullet_dir = (
     }
   
     void Start()
